6dd380fd55b4dfbe1d05de61c039cc65e9f53489
Dez Fedsie <79500624+miscellaneous-mice@users.noreply.github.com>
2023-10-22 23:25:48 +0530

    Update xmonad.hs

--- nord/.xmonad/xmonad.hs
+++ nord/.xmonad/xmonad.hs
@@ -71,7 +71,7 @@ myModMask = mod1Mask
 -- > workspaces = ["web", "irc", "code" ] ++ map show [4..9]
 --
 myWorkspaces :: [[Char]]
-myWorkspaces = ["1","2","3","4","5","6","7","8","9"]
+myWorkspaces = ["1","2","3","4","5","6","7","8"]
 
 -- Border colors for unfocused and focused windows, respectively.
 --
